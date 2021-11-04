ab5d9bdcc26412d3df0dfce0403c192bfec824a4
“Nayeem-Bhuiyangit config --global user.email “nayeembhuiyan40@gmail.com <58185521+Nayeem-Bhuiyan@users.noreply.github.com>
2021-11-04 09:29:36 +0600

    mig

---
+++ RestaurantManagement/Migrations/20211104032851_BoucherSell.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RestaurantManagement.Migrations
+{
+    public partial class BoucherSell : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
